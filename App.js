4c8d6d93ef7177ab697ea5ad4f3cd7d91c3050a5
marte1309 <mrd1309@gmail.com>
2020-10-12 10:54:30 -0500

    OneSignal

--- App.js
+++ App.js
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Routes from './src/core/routes';
+import OneSignal from 'react-native-onesignal';
 
-export default function App() {
-	return (
-		<Routes/>
-	);
+class App extends Component {
+	constructor(props) {
+		super(props);
+		// OneSignal.setLogLevel(OneSignal.LOG_LEVEL.DEBUG, OneSignal.LOG_LEVEL.DEBUG);
+		OneSignal.init("ee672dec-e2d4-426a-8cd0-c8fa3ba8c154", {
+			kOSSettingsKeyAutoPrompt: false, 
+            kOSSettingsKeyInAppLaunchURL: false
+		});
+		OneSignal.inFocusDisplaying(2);
+		OneSignal.requestPermissions();
+		OneSignal.addEventListener('received', this.onReceived);
+		OneSignal.removeEventListener('opened', this.onOpened);
+		OneSignal.addEventListener('ids', this.onIds);
+	}
+
+	componentWillUnmount() {
+		OneSignal.removeEventListener('received', this.onReceived);
+		OneSignal.removeEventListener('opened', this.onOpened);
+		OneSignal.removeEventListener('ids', this.onIds);
+	}
+
+	onOpened(openResult) {
+		console.log('Message: ', openResult.notification.payload.body);
+		console.log('Data: ', openResult.notification.payload.additionalData);
+		console.log('isActive: ', openResult.notification.isAppInFocus);
+		console.log('openResult: ', openResult);
+	}
+
+	onReceived(notification) {
+		console.log("Notification received: ", notification);
+	}
+
+	onIds(device) {
+		console.log('Device info: ', device);
+	}
+
+	render() {
+		return (
+			<Routes/>
+		)
+	}
 }
+
+export default App;
