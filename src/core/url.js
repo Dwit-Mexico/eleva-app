a109faa7de316986420f8ee0a012840be3a9879a
marte1309 <mrd1309@gmail.com>
2020-10-14 11:00:41 -0500

    api de test

--- src/core/url.js
+++ src/core/url.js
@@ -3,5 +3,5 @@ export let API_URL;
 if(process.env.NODE_ENV !== 'production') {
 	API_URL 	= 	"http://192.168.1.189:8081";
 } else {
-	API_URL 	= 	"https://eleva.herokuapp.com";
+	API_URL 	= 	"https://elevatest.herokuapp.com";
 }
