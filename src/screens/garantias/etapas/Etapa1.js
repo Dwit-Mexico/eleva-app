aef7ef2eb418a6e67e584d9543ba26ff4dc5e6d5
marte1309 <mrd1309@gmail.com>
2020-10-14 16:49:52 -0500

    Etapa 1

--- src/screens/garantias/etapas/Etapa1.js
+++ src/screens/garantias/etapas/Etapa1.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { ScrollView, Text, View, TextInput } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 //Componentes
 import SelectVivienda from '../../../components/select/SelectVivienda';
@@ -9,7 +10,7 @@ import SelectProblema from '../../../components/select/SelectProblema';
 
 function Etapa1() {
 	return (
-		<>
+		<ScrollView>
 			<Text>Etapa 1</Text>
 			<View style={{height: 8}}/>
 			<SelectVivienda onChange = {(data) => console.log('SelectVivienda', data)}/>
@@ -19,7 +20,23 @@ function Etapa1() {
 			<SelectObjeto onChange = {(data) => console.log('SelectObjeto', data)}/>
 			<View style={{height: 8}}/>
 			<SelectProblema onChange = {(data) => console.log('SelectProblema', data)}/>
-		</>
+			<View style={{height: 8}}/>
+			<TextInput
+				placeholder="< Escriba sus comentarios >"
+				style={{borderRadius: 8, borderColor: '#000', borderWidth: 1, paddingVertical: 10, paddingHorizontal: 17, textAlignVertical: "top",
+				fontSize: 16,}}
+				multiline
+				numberOfLines={6}/>
+			<View style={{height: 16}}/>
+			<View style={{flexDirection: 'row', justifyContent: 'space-between', width: '100%'}}>
+				<FontAwesome5 name="camera" size={30}/>
+				<View style={{flexDirection: 'row', justifyContent: 'space-between', width: '50%'}}>
+					<FontAwesome5 name="image" size={30}/>
+					<FontAwesome5 name="image" size={30}/>
+					<FontAwesome5 name="image" size={30}/>
+				</View>
+			</View>
+		</ScrollView>
 	);
 }
 
