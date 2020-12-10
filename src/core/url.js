dd3aff78bd06643ab7e1e232f5f4fbcedd975981
marte1309 <mrd1309@gmail.com>
2020-12-10 11:42:20 -0500

    build produccion

--- src/core/url.js
+++ src/core/url.js
@@ -4,5 +4,6 @@ if(process.env.NODE_ENV !== 'production') {
 	API_URL 	= 	"http://192.168.1.74:8082";
 	//API_URL 	= 	"http://192.168.1.102:8082";
 } else {
-	API_URL 	= 	"https://eleva-test.herokuapp.com";
+	//API_URL 	= 	"https://eleva-test.herokuapp.com";
+	API_URL		=	"https://eleva-cap.herokuapp.com";
 }
