b38280c10c7c59ea1772595fc2782a332a04f65f
LAPTOP-GUE0BER0\Appmosphera <luis@dwitmexico.com>
2022-12-05 20:43:25 -0500

    build apk

--- src/core/url.js
+++ src/core/url.js
@@ -2,9 +2,9 @@ export let API_URL;
 
 if (process.env.NODE_ENV !== 'production') {
 	//API_URL = "http://192.168.1.96:8082";
-	API_URL = "https://eleva-test.herokuapp.com";
-	//API_URL = "https://eleva-cap.herokuapp.com";
+	//API_URL = "https://eleva-test.herokuapp.com";
+	API_URL = "https://eleva-cap.herokuapp.com";
 } else {
 	//API_URL = "https://eleva-test.herokuapp.com";
-	//API_URL = "https://eleva-cap.herokuapp.com";
+	API_URL = "https://eleva-cap.herokuapp.com";
 }
