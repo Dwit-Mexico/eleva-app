5a142e67bc85045258340f56cb4b3768aedfaefd
Itzel Ramon <itzelramonf@gmail.com>
2024-07-03 10:25:40 -0500

    update expo and dependencies

--- src/context/index.js
+++ src/context/index.js
@@ -1,172 +1,177 @@
-import React, { Component, createContext } from 'react';
-import { AsyncStorage } from 'react-native';
+import React, {Component, createContext} from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 //Funciones
-import { login, logout, validar } from './User';
-import { getUnidades, getSetUnidades } from './catalogos/Unidades';
-import { getAreas } from './catalogos/Areas';
-import { getEquipos } from './catalogos/Equipos';
-import { getProblemas } from './catalogos/Problemas';
-import { setStep } from './catalogos/Wizard';
+import {login, logout, validar} from "./User";
+import {getUnidades, getSetUnidades} from "./catalogos/Unidades";
+import {getAreas} from "./catalogos/Areas";
+import {getEquipos} from "./catalogos/Equipos";
+import {getProblemas} from "./catalogos/Problemas";
+import {setStep} from "./catalogos/Wizard";
 import {
-	getForm,
-	setUnidad,
-	setArea,
-	setEquipo,
-	setProblema,
-	setComentario,
-	setImagen1,
-	setImagen2,
-	setImagen3,
-	setVideo1
-} from './form';
-import { getReportes, reloadReportes, getReportesAgrupados, reloadReportesAgrupados } from './reportes';
+   getForm,
+   setUnidad,
+   setArea,
+   setEquipo,
+   setProblema,
+   setComentario,
+   setImagen1,
+   setImagen2,
+   setImagen3,
+   setVideo1,
+} from "./form";
+import {
+   getReportes,
+   reloadReportes,
+   getReportesAgrupados,
+   reloadReportesAgrupados,
+} from "./reportes";
 
 const Context = createContext();
 
 class GlobalContext extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			initApp: this.initApp.bind(this),
-			auth: false,
-			user: null,
-			token: null,
-			login: login.bind(this),
-			validar: validar.bind(this),
-			logout: logout.bind(this),
-			unidades: [],
-			areas: [],
-			equipos: [],
-			problemas: [],
-			getSetUnidades: getSetUnidades.bind(this),
-			getAreas: getAreas.bind(this),
-			getEquipos: getEquipos.bind(this),
-			getProblemas: getProblemas.bind(this),
-			step: 1,
-			setStep: setStep.bind(this),
-			unidad: null,
-			setUnidad: setUnidad.bind(this),
-			area: null,
-			setArea: setArea.bind(this),
-			equipo: null,
-			setEquipo: setEquipo.bind(this),
-			problema: null,
-			setProblema: setProblema.bind(this),
-			comentario: null,
-			setComentario: setComentario.bind(this),
-			imagen1: null,
-			setImagen1: setImagen1.bind(this),
-			imagen2: null,
-			setImagen2: setImagen2.bind(this),
-			imagen3: null,
-			setImagen3: setImagen3.bind(this),
-			video1: null,
-			setVideo1: setVideo1.bind(this),
-			getForm: getForm.bind(this),
-			reportes: [],
-			reportesAgrupados: [],
-			getReportes: getReportes.bind(this),
-			reloadReportes: reloadReportes.bind(this),
-			getReportesAgrupados: getReportesAgrupados.bind(this),
-			reloadReportesAgrupados: reloadReportesAgrupados.bind(this)
-		}
+   constructor(props) {
+      super(props);
+      this.state = {
+         initApp: this.initApp.bind(this),
+         auth: false,
+         user: null,
+         token: null,
+         login: login.bind(this),
+         validar: validar.bind(this),
+         logout: logout.bind(this),
+         unidades: [],
+         areas: [],
+         equipos: [],
+         problemas: [],
+         getSetUnidades: getSetUnidades.bind(this),
+         getAreas: getAreas.bind(this),
+         getEquipos: getEquipos.bind(this),
+         getProblemas: getProblemas.bind(this),
+         step: 1,
+         setStep: setStep.bind(this),
+         unidad: null,
+         setUnidad: setUnidad.bind(this),
+         area: null,
+         setArea: setArea.bind(this),
+         equipo: null,
+         setEquipo: setEquipo.bind(this),
+         problema: null,
+         setProblema: setProblema.bind(this),
+         comentario: null,
+         setComentario: setComentario.bind(this),
+         imagen1: null,
+         setImagen1: setImagen1.bind(this),
+         imagen2: null,
+         setImagen2: setImagen2.bind(this),
+         imagen3: null,
+         setImagen3: setImagen3.bind(this),
+         video1: null,
+         setVideo1: setVideo1.bind(this),
+         getForm: getForm.bind(this),
+         reportes: [],
+         reportesAgrupados: [],
+         getReportes: getReportes.bind(this),
+         reloadReportes: reloadReportes.bind(this),
+         getReportesAgrupados: getReportesAgrupados.bind(this),
+         reloadReportesAgrupados: reloadReportesAgrupados.bind(this),
+      };
 
-		this.initUser();
-	}
+      this.initUser();
+   }
 
-	async componentDidMount() {
-		await this.initUser();
-		// await this.initApp();
-	}
+   async componentDidMount() {
+      await this.initUser();
+      // await this.initApp();
+   }
 
-	async componentDidUpdate(prevProps, prevState) {
-		if (this.state.token !== prevState.token) {
-			this.initApp();
-		}
-	}
+   async componentDidUpdate(prevProps, prevState) {
+      if (this.state.token !== prevState.token) {
+         this.initApp();
+      }
+   }
 
-	async initUser() {
-		//Comprobar si el usuario inició sesión
-		let loginUser = await AsyncStorage.getItem('LoginUser');
-		if (loginUser) {
-			loginUser = JSON.parse(loginUser);
-			if (loginUser.token) {
-				this.setState({ auth: true, token: loginUser.token });
-			}
-		}
-	}
+   async initUser() {
+      //Comprobar si el usuario inició sesión
+      let loginUser = await AsyncStorage.getItem("LoginUser");
+      if (loginUser) {
+         loginUser = JSON.parse(loginUser);
+         if (loginUser.token) {
+            this.setState({auth: true, token: loginUser.token});
+         }
+      }
+   }
 
-	async initApp() {
-		let catalogos = await AsyncStorage.getItem('Catalogos');
-		if (catalogos) {
-			catalogos = JSON.parse(catalogos);
-			this.setState({
-				areas: catalogos.areas,
-				objetos: catalogos.objetos,
-				problemas: catalogos.problemas,
-				reportes: catalogos.reportes
-			});
-		}
+   async initApp() {
+      console.log("antes de log");
+      let catalogos = await AsyncStorage.getItem("Catalogos");
+      console.log("despues de catalogoObj");
 
+      if (catalogos) {
+         catalogos = JSON.parse(catalogos);
+         this.setState({
+            areas: catalogos.areas,
+            objetos: catalogos.objetos,
+            problemas: catalogos.problemas,
+            reportes: catalogos.reportes,
+         });
+      }
 
-		const catalogoObj = {
-			areas: [],
-			objetos: [],
-			equipos: []
-		};
+      const catalogoObj = {
+         areas: [],
+         objetos: [],
+         equipos: [],
+      };
 
-		const unidades = await getUnidades();
-		if (unidades.data) {
+      const unidades = await getUnidades();
+      if (unidades.data) {
+         this.setState({unidades: unidades.data});
 
-			this.setState({ unidades: unidades.data });
+         if (Array.isArray(unidades.data)) {
+            catalogoObj.unidades = unidades.data;
 
-			if (Array.isArray(unidades.data)) {
+            if (unidades.data.length == 1) {
+               const unidad = unidades.data[0];
+            }
+         }
+      }
 
-				catalogoObj.unidades = unidades.data;
-
-				if (unidades.data.length == 1) {
-					const unidad = unidades.data[0];
-				}
-			}
-		}
-
-		/* const problemas = await getProblemas();
+      /* const problemas = await getProblemas();
 		if (problemas.problemas) {
 			catalogoObj.problemas = problemas.problemas;
 			this.setState({ problemas: problemas.problemas });
 		} */
 
-		const reportes = await this.state.getReportes();
-		if (reportes.data) {
-			catalogoObj.reportes = reportes.data;
-		}
+      const reportes = await this.state.getReportes();
+      if (reportes.data) {
+         catalogoObj.reportes = reportes.data;
+      }
 
-		const reportesAgrupados = await this.state.getReportesAgrupados();
-		if (reportesAgrupados.data) {
-			catalogoObj.reportesAgrupados = reportesAgrupados.data;
-		}
+      const reportesAgrupados = await this.state.getReportesAgrupados();
+      if (reportesAgrupados.data) {
+         catalogoObj.reportesAgrupados = reportesAgrupados.data;
+      }
 
-		AsyncStorage.setItem('Catalogos', JSON.stringify(catalogoObj));
-	}
+      AsyncStorage.setItem("Catalogos", JSON.stringify(catalogoObj));
+   }
 
-	render() {
-		return (
-			<Context.Provider value={this.state}>
-				{this.props.children}
-			</Context.Provider>
-		)
-	}
+   render() {
+      return (
+         <Context.Provider value={this.state}>
+            {this.props.children}
+         </Context.Provider>
+      );
+   }
 }
 
-const Consumer = Component => {
-	return (props) => {
-		return (
-			<Context.Consumer>
-				{context => <Component {...props} context={context} />}
-			</Context.Consumer>
-		)
-	}
-}
+const Consumer = (Component) => {
+   return (props) => {
+      return (
+         <Context.Consumer>
+            {(context) => <Component {...props} context={context} />}
+         </Context.Consumer>
+      );
+   };
+};
 
-export { GlobalContext, Consumer };
+export {GlobalContext, Consumer};
--- src/core/api.js
+++ src/core/api.js
@@ -1,159 +1,166 @@
-import { AsyncStorage, Platform } from 'react-native';
-import request from 'superagent';
-import { API_URL } from './url';
+import {Platform} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import request from "superagent";
+import {API_URL} from "./url";
 import ExpoInfo from "expo-constants";
 
-let version_app = '1.0.0';
+let version_app = "1.0.0";
 
 class Request {
-	constructor() {
-		this.state = {
-			auth: ''
-		}
+   constructor() {
+      this.state = {
+         auth: "",
+      };
 
-		if (ExpoInfo.manifest) {
-			const {version} = ExpoInfo.manifest;
-			version_app = version;
-		}
-	}
+      if (ExpoInfo.manifest) {
+         const {version} = ExpoInfo.manifest;
+         version_app = version;
+      }
+   }
 
-	async get(method, data) {
-		let auth = '';
-		let loginUser = await AsyncStorage.getItem('LoginUser');
+   async get(method, data) {
+      let auth = "";
+      let loginUser = await AsyncStorage.getItem("LoginUser");
 
-		if (loginUser) {
-			loginUser = JSON.parse(loginUser);
-			auth = loginUser.token;
-		}
+      if (loginUser) {
+         loginUser = JSON.parse(loginUser);
+         auth = loginUser.token;
+      }
 
-        const res = request
-            .get(`${API_URL}${method}`)
-            .query(data)
-			.set('api_key', '87882e138de18177515be74e7e098cd81a79cc44fcfb0097e55230b2280df6b1')
-			.set('auth', 	auth)
-			.set('Accept', 	'application/json')
-			.set("app_platform", Platform.OS)
-			.set("app_version", version_app)
-            .then(res => {
-                return res.body || { empty: true };
-            })
-            .catch(err => {
-				if (err.status === 403) {
-					err.message = 'Error de permisos.';
-				}
-				if (err.message) {
-					if (err.message.indexOf("Access-Control-Allow-Origin") !== -1)
-						return {
-							error: true,
-							timeout: true,
-							message: "Ups! Parace que no hay conexión con el servidor, compruebe que este conectado a internet y pruebe nuevamente.",
-						};
-					else
-						return { error: true, message: err.message };
-				} else 
-					return { error: true, message: "Error de conexión" };
+      const res = request
+         .get(`${API_URL}${method}`)
+         .query(data)
+         .set(
+            "api_key",
+            "87882e138de18177515be74e7e098cd81a79cc44fcfb0097e55230b2280df6b1"
+         )
+         .set("auth", auth)
+         .set("Accept", "application/json")
+         .set("app_platform", Platform.OS)
+         .set("app_version", version_app)
+         .then((res) => {
+            return res.body || {empty: true};
+         })
+         .catch((err) => {
+            if (err.status === 403) {
+               err.message = "Error de permisos.";
+            }
+            if (err.message) {
+               if (err.message.indexOf("Access-Control-Allow-Origin") !== -1)
+                  return {
+                     error: true,
+                     timeout: true,
+                     message:
+                        "Ups! Parace que no hay conexión con el servidor, compruebe que este conectado a internet y pruebe nuevamente.",
+                  };
+               else return {error: true, message: err.message};
+            } else return {error: true, message: "Error de conexión"};
+         });
+      return res;
+   }
+
+   async post(method, data) {
+      let auth = "";
+      let loginUser = await AsyncStorage.getItem("LoginUser");
+
+      if (loginUser) {
+         loginUser = JSON.parse(loginUser);
+         auth = loginUser.token;
+      }
+
+      const res = request
+         .post(`${API_URL}${method}`)
+         .set(
+            "api_key",
+            "87882e138de18177515be74e7e098cd81a79cc44fcfb0097e55230b2280df6b1"
+         )
+         .set("auth", auth)
+         .set("Accept", "application/json")
+         .set("Content-Type", "application/json")
+         .set("app_platform", Platform.OS)
+         .set("app_version", version_app)
+         .send(data)
+         .then((res) => {
+            return res.body || {empty: true};
+         })
+         .catch((err) => {
+            if (err.status === 403) {
+               err.message = "Error de permisos.";
+            }
+            if (err.message) {
+               if (err.message.indexOf("Access-Control-Allow-Origin") !== -1)
+                  return {
+                     error: true,
+                     timeout: true,
+                     message:
+                        "Ups! Parace que no hay conexión con el servidor, compruebe que este conectado a internet y pruebe nuevamente.",
+                  };
+               else return {error: true, message: err.message};
+            } else return {error: true, message: "Error de conexión"};
+         });
+      return res;
+   }
+
+   async postFile(method, files, data) {
+      let auth = "";
+      let loginUser = await AsyncStorage.getItem("LoginUser");
+
+      if (loginUser) {
+         loginUser = JSON.parse(loginUser);
+         auth = loginUser.token;
+      }
+
+      const response = await new Promise((res) => {
+         const postRequest = request.post(`${API_URL}${method}`);
+
+         if (Array.isArray(files)) {
+            files.forEach((file, index) => {
+               if (file) {
+                  postRequest.attach(`file_${index + 1}`, file);
+               }
             });
-        return res;
-	}
+         }
+         if (data) {
+            const keys = Object.keys(data);
+            keys.forEach((key) => {
+               if (data[key] !== undefined) {
+                  postRequest.field(key, data[key]);
+               }
+            });
+         }
 
-	async post(method, data) {
-		let auth = '';
-		let loginUser = await AsyncStorage.getItem('LoginUser');
-
-		if (loginUser) {
-			loginUser = JSON.parse(loginUser);
-			auth = loginUser.token;
-		}
-
-        const res = request
-            .post(`${API_URL}${method}`)
-			.set('api_key', '87882e138de18177515be74e7e098cd81a79cc44fcfb0097e55230b2280df6b1')
-			.set('auth', auth)
+         postRequest
+            .set(
+               "api_key",
+               "87882e138de18177515be74e7e098cd81a79cc44fcfb0097e55230b2280df6b1"
+            )
+            .set("auth", auth)
             .set("Accept", "application/json")
-			.set("Content-Type", "application/json")
-			.set("app_platform", Platform.OS)
-			.set("app_version", version_app)
-			.send(data)
-            .then(res => {
-                return res.body || { empty: true };
+            .set("app_platform", Platform.OS)
+            .set("app_version", version_app)
+            .then((resp) => {
+               res(resp.body || {error: true, message: "error indefinido"});
             })
-            .catch(err => {
-				if (err.status === 403) {
-					err.message = 'Error de permisos.';
-				}
-				if (err.message) {
-					if (err.message.indexOf("Access-Control-Allow-Origin") !== -1)
-						return {
-							error: true,
-							timeout: true,
-							message: "Ups! Parace que no hay conexión con el servidor, compruebe que este conectado a internet y pruebe nuevamente.",
-						};
-					else
-						return { error: true, message: err.message };
-				} else 
-					return { error: true, message: "Error de conexión" };
-			});
-        return res;
-	}
+            .catch((err) => {
+               if (err.status === 403) {
+                  err.message = "Error de permisos.";
+               }
+               if (err.message) {
+                  if (err.message.indexOf("Access-Control-Allow-Origin") !== -1)
+                     res({
+                        error: true,
+                        timeout: true,
+                        message:
+                           "Ups! Parace que no hay conexión con el servidor, compruebe que este conectado a internet y pruebe nuevamente.",
+                     });
+                  else res({error: true, message: err.message});
+               } else res({error: true, message: "Error de conexión"});
+            });
+      });
 
-	async postFile(method, files, data) {
-		let auth = '';
-		let loginUser = await AsyncStorage.getItem('LoginUser');
-
-		if (loginUser) {
-			loginUser = JSON.parse(loginUser);
-			auth = loginUser.token;
-		}
-
-		const response = await new Promise(res => {
-			const postRequest = request.post(`${API_URL}${method}`);
-
-			if (Array.isArray(files)) {
-				files.forEach((file, index) => {
-					if (file) {
-						postRequest.attach(`file_${index + 1}`, file);
-					}
-				})
-			}
-			if (data) {
-				const keys = Object.keys(data);
-				keys.forEach(key => {
-					if (data[key] !== undefined) {
-						postRequest.field(key, data[key]);
-					}
-				})
-			}
-
-			postRequest
-				.set('api_key', '87882e138de18177515be74e7e098cd81a79cc44fcfb0097e55230b2280df6b1')
-				.set('auth', auth)
-				.set('Accept', 'application/json')
-				.set("app_platform", Platform.OS)
-				.set("app_version", version_app)
-				.then(resp => {
-					res(resp.body || { error: true, message: 'error indefinido' });
-				})
-				.catch(err => {
-					if (err.status === 403) {
-						err.message = 'Error de permisos.';
-					}
-					if (err.message) {
-						if (err.message.indexOf("Access-Control-Allow-Origin") !== -1)
-							res( {
-								error: true,
-								timeout: true,
-								message: "Ups! Parace que no hay conexión con el servidor, compruebe que este conectado a internet y pruebe nuevamente.",
-							} );
-						else
-							res( { error: true, message: err.message } );
-					} else 
-						res( { error: true, message: "Error de conexión" } );
-				});
-		});
-
-		console.log('RESPONSE', response);
-		return response;
-	}
+      console.log("RESPONSE", response);
+      return response;
+   }
 }
 
-export default Request
+export default Request;
