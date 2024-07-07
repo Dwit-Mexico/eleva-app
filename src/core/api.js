b7d192b449b0d23ced281eb37d2a0a42f6c2c2b8
Diego <diegogutcat28@gmail.com>
2024-07-07 14:26:14 -0500

    deleted expo-permissions in order to use in-build plugins permissions

--- src/core/api.js
+++ src/core/api.js
@@ -1,171 +1,167 @@
-import {Platform} from "react-native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import request from "superagent";
-import {API_URL} from "./url";
-import Constants from "expo-constants";
+import { Platform } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import request from 'superagent';
+import { API_URL } from './url';
+import Constants from 'expo-constants';
 
-let version_app = "1.0.0";
+let version_app = '1.0.0';
 
 class Request {
-   constructor() {
-      this.state = {
-         auth: "",
-      };
+  constructor() {
+    this.state = {
+      auth: '',
+    };
 
-      if (Constants.expoConfig) {
-         const {version} = Constants.expoConfig;
-         version_app = version;
-      }
-   }
+    if (Constants.expoConfig) {
+      const { version } = Constants.expoConfig;
+      version_app = version;
+    }
+  }
 
-   async get(method, data) {
-      let auth = "";
-      let loginUser = await AsyncStorage.getItem("LoginUser");
+  async get(method, data) {
+    let auth = '';
+    let loginUser = await AsyncStorage.getItem('LoginUser');
 
-      if (loginUser) {
-         loginUser = JSON.parse(loginUser);
-         auth = loginUser.token;
-      }
+    if (loginUser) {
+      loginUser = JSON.parse(loginUser);
+      auth = loginUser.token;
+    }
 
-      const res = request
-         .get(`${API_URL}${method}`)
-         .query(data)
-         .set(
-            "api_key",
-            "87882e138de18177515be74e7e098cd81a79cc44fcfb0097e55230b2280df6b1"
-         )
-         .set("auth", auth)
-         .set("Accept", "application/json")
-         .set("app_platform", Platform.OS)
-         .set("app_version", version_app)
-         .then((res) => {
-            return res.body || {empty: true};
-         })
-         .catch((err) => {
-            if (err.status === 403) {
-               err.message = "Error de permisos.";
-            }
-            if (err.message) {
-               if (err.message.indexOf("Access-Control-Allow-Origin") !== -1)
-                  return {
-                     error: true,
-                     timeout: true,
-                     message:
-                        "Ups! Parace que no hay conexión con el servidor, compruebe que este conectado a internet y pruebe nuevamente.",
-                  };
-               else return {error: true, message: err.message};
-            } else return {error: true, message: "Error de conexión"};
-         });
-      return res;
-   }
-
-   async post(method, data) {
-      let auth = "";
-      let loginUser = await AsyncStorage.getItem("LoginUser");
-
-      if (loginUser) {
-         loginUser = JSON.parse(loginUser);
-         auth = loginUser.token;
-      }
-
-      const res = request
-         .post(`${API_URL}${method}`)
-         .set(
-            "api_key",
-            "87882e138de18177515be74e7e098cd81a79cc44fcfb0097e55230b2280df6b1"
-         )
-         .set("auth", auth)
-         .set("Accept", "application/json")
-         .set("Content-Type", "application/json")
-         .set("app_platform", Platform.OS)
-         .set("app_version", version_app)
-         .send(data)
-         .then((res) => {
-            return res.body || {empty: true};
-         })
-         .catch((err) => {
-            if (err.status === 403) {
-               err.message = "Error de permisos.";
-            }
-            if (err.message) {
-               if (err.message.indexOf("Access-Control-Allow-Origin") !== -1)
-                  return {
-                     error: true,
-                     timeout: true,
-                     message:
-                        "Ups! Parace que no hay conexión con el servidor, compruebe que este conectado a internet y pruebe nuevamente.",
-                  };
-               else return {error: true, message: err.message};
-            } else return {error: true, message: "Error de conexión"};
-         });
-      return res;
-   }
-
-   async postFile(method, files, data) {
-      let auth = "";
-      let loginUser = await AsyncStorage.getItem("LoginUser");
-
-      if (loginUser) {
-         loginUser = JSON.parse(loginUser);
-         auth = loginUser.token;
-      }
-
-      const response = await new Promise((res) => {
-         console.log(`${API_URL}${method}`);
-         const postRequest = request.post(`${API_URL}${method}`);
-
-         if (Array.isArray(files)) {
-            files.forEach((file, index) => {
-               if (file) {
-                  postRequest.attach(`file_${index + 1}`, file);
-               }
-            });
-         }
-
-         if (data) {
-            const keys = Object.keys(data);
-            keys.forEach((key) => {
-               if (data[key] !== undefined) {
-                  postRequest.field(key, data[key]);
-               }
-            });
-         }
-         console.log("postRequest", postRequest);
-         postRequest
-            .set(
-               "api_key",
-               "87882e138de18177515be74e7e098cd81a79cc44fcfb0097e55230b2280df6b1"
-            )
-            .set("auth", auth)
-            .set("Accept", "application/json")
-            .set("Content-Type", "application/json")
-            .set("app_platform", Platform.OS)
-            .set("app_version", version_app)
-            .send(data)
-            .then((resp) => {
-               res(resp.body || {error: true, message: "error indefinido"});
-            })
-            .catch((err) => {
-               console.log("ERROR", err);
-               if (err.status === 403) {
-                  err.message = "Error de permisos.";
-               }
-               if (err.message) {
-                  if (err.message.indexOf("Access-Control-Allow-Origin") !== -1)
-                     res({
-                        error: true,
-                        timeout: true,
-                        message:
-                           "Ups! Parace que no hay conexión con el servidor, compruebe que este conectado a internet y pruebe nuevamente.",
-                     });
-                  else res({error: true, message: err.message});
-               } else res({error: true, message: "Error de conexión"});
-            });
+    const res = request
+      .get(`${API_URL}${method}`)
+      .query(data)
+      .set('api_key', '87882e138de18177515be74e7e098cd81a79cc44fcfb0097e55230b2280df6b1')
+      .set('auth', auth)
+      .set('Accept', 'application/json')
+      .set('app_platform', Platform.OS)
+      .set('app_version', version_app)
+      .then(res => {
+        return res.body || { empty: true };
+      })
+      .catch(err => {
+        if (err.status === 403) {
+          err.message = 'Error de permisos.';
+        }
+        if (err.message) {
+          if (err.message.indexOf('Access-Control-Allow-Origin') !== -1)
+            return {
+              error: true,
+              timeout: true,
+              message:
+                'Ups! Parace que no hay conexión con el servidor, compruebe que este conectado a internet y pruebe nuevamente.',
+            };
+          else return { error: true, message: err.message };
+        } else return { error: true, message: 'Error de conexión' };
       });
+    return res;
+  }
 
-      console.log("RESPONSE", response);
-      return response;
-   }
+  async post(method, data) {
+    let auth = '';
+    let loginUser = await AsyncStorage.getItem('LoginUser');
+
+    if (loginUser) {
+      loginUser = JSON.parse(loginUser);
+      auth = loginUser.token;
+    }
+
+    const res = request
+      .post(`${API_URL}${method}`)
+      .set('api_key', '87882e138de18177515be74e7e098cd81a79cc44fcfb0097e55230b2280df6b1')
+      .set('auth', auth)
+      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json')
+      .set('app_platform', Platform.OS)
+      .set('app_version', version_app)
+      .send(data)
+      .then(res => {
+        return res.body || { empty: true };
+      })
+      .catch(err => {
+        if (err.status === 403) {
+          err.message = 'Error de permisos.';
+        }
+        if (err.message) {
+          if (err.message.indexOf('Access-Control-Allow-Origin') !== -1)
+            return {
+              error: true,
+              timeout: true,
+              message:
+                'Ups! Parace que no hay conexión con el servidor, compruebe que este conectado a internet y pruebe nuevamente.',
+            };
+          else return { error: true, message: err.message };
+        } else return { error: true, message: 'Error de conexión' };
+      });
+    return res;
+  }
+
+  async postFile(method, files, data) {
+    console.log(files);
+    console.log(data);
+    let auth = '';
+    let loginUser = await AsyncStorage.getItem('LoginUser');
+
+    if (loginUser) {
+      loginUser = JSON.parse(loginUser);
+      auth = loginUser.token;
+    }
+
+    const response = await new Promise(res => {
+      console.log(`${API_URL}${method}`);
+      const postRequest = request.post(`${API_URL}${method}`);
+
+      if (Array.isArray(files)) {
+        files.forEach((file, index) => {
+          if (file && file.uri && file.uri.uri) {
+            postRequest.attach(`file_${index + 1}`, {
+              uri: file.uri.uri,
+              name: file.name,
+              type: file.type,
+            });
+          }
+        });
+      }
+
+      if (data) {
+        const keys = Object.keys(data);
+        keys.forEach(key => {
+          if (data[key] !== undefined) {
+            postRequest.field(key, data[key]);
+          }
+        });
+      }
+      console.log('postRequest', postRequest);
+      postRequest
+        .set('api_key', '87882e138de18177515be74e7e098cd81a79cc44fcfb0097e55230b2280df6b1')
+        .set('auth', auth)
+        .set('Accept', 'application/json')
+        .set('Content-Type', 'multipart/form-data')
+        .set('app_platform', Platform.OS)
+        .set('app_version', version_app)
+        .then(resp => {
+          res(resp.body || { error: true, message: 'error indefinido' });
+        })
+        .catch(err => {
+          console.log('ERROR', err);
+          if (err.status === 403) {
+            err.message = 'Error de permisos.';
+          }
+          if (err.message) {
+            if (err.message.indexOf('Access-Control-Allow-Origin') !== -1)
+              res({
+                error: true,
+                timeout: true,
+                message:
+                  'Ups! Parace que no hay conexión con el servidor, compruebe que este conectado a internet y pruebe nuevamente.',
+              });
+            else res({ error: true, message: err.message });
+          } else res({ error: true, message: 'Error de conexión' });
+        });
+    });
+
+    console.log('RESPONSE', response);
+    return response;
+  }
 }
 
 export default Request;
--- src/navigation/AppStack.js
+++ src/navigation/AppStack.js
@@ -5,7 +5,6 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { Consumer } from '../context';
 import Constants from 'expo-constants';
 import * as Notifications from 'expo-notifications';
-import * as Permissions from 'expo-permissions';
 import Request from '../core/api';
 
 //Componentes
@@ -47,38 +46,38 @@ import Camara from '../screens/camara';
 const request = new Request();
 
 Notifications.setNotificationHandler({
-	handleNotification: async () => ({
-		shouldShowAlert: true,
-		shouldPlaySound: false,
-		shouldSetBadge: false,
-	}),
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
 });
 
 function getHeaderTitle(route) {
-	const routeName = getFocusedRouteNameFromRoute(route) ?? 'reportes';
+  const routeName = getFocusedRouteNameFromRoute(route) ?? 'reportes';
 
-  	switch (routeName) {
-		case 'perfil':
-			return 'Perfil';
-		case 'reportes':
-			return 'Customer Service';
-		case 'documentos':
-			return 'Documentos';
-		case 'galeria':
-			return 'Galería';
-	}
+  switch (routeName) {
+    case 'perfil':
+      return 'Perfil';
+    case 'reportes':
+      return 'Customer Service';
+    case 'documentos':
+      return 'Documentos';
+    case 'galeria':
+      return 'Galería';
+  }
 }
 
 async function registerForPushNotificationsAsync() {
-	let token;
-	if (Constants.isDevice) {
-		const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
-		let finalStatus = existingStatus;
-		if (existingStatus !== 'granted') {
-			const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-			finalStatus = status;
-		}
-		/* if (finalStatus !== 'granted') {
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    /* if (finalStatus !== 'granted') {
 			if (Platform.OS == 'ios') {
 				Alert.alert(
 					'Permisos',
@@ -98,258 +97,277 @@ async function registerForPushNotificationsAsync() {
 			}
 			return;
 		} */
-		token = (await Notifications.getExpoPushTokenAsync()).data;
-	} else {
-		Alert.alert(null, 'Solo dispositivos fisicos pueden recibir notificaciones');
-	}
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+  } else {
+    Alert.alert(null, 'Solo dispositivos fisicos pueden recibir notificaciones');
+  }
 
-	if (Platform.OS === 'android') {
-		Notifications.setNotificationChannelAsync('default', {
-			name: 'default',
-			importance: Notifications.AndroidImportance.MAX,
-			vibrationPattern: [0, 250, 250, 250],
-			lightColor: '#FF231F7C',
-		});
-	}
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
 
-	return token;
+  return token;
 }
 
 const Stack = createStackNavigator();
 
 function AppStack(props) {
-	const [expoPushToken, setExpoPushToken] = useState('');
+  const [expoPushToken, setExpoPushToken] = useState('');
 
-	useEffect(() => {
-		StatusBar.setBarStyle('light-content');
+  useEffect(() => {
+    StatusBar.setBarStyle('light-content');
 
-		registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
-	}, []);
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+  }, []);
 
-	useEffect(() => {
-		async function setToken(token) {
-			const response = await request.post('/aplicacion/notificaciones/set', { token });
-			// console.log('PUSH TOKEN', response);
-		}
+  useEffect(() => {
+    async function setToken(token) {
+      const response = await request.post('/aplicacion/notificaciones/set', { token });
+      // console.log('PUSH TOKEN', response);
+    }
 
-		if (expoPushToken) {
-			setToken(expoPushToken);
-		}
-	}, [expoPushToken]);
+    if (expoPushToken) {
+      setToken(expoPushToken);
+    }
+  }, [expoPushToken]);
 
-	return (
-		<Stack.Navigator mode="modal">
-			<Stack.Screen
-				name="Main"
-				component={BottomNavigator}
-				options={({ navigation, route }) => ({
-					// header: () => null,
-					headerTitle: getHeaderTitle(route),
-					headerRight: () => <BotonNotificaciones navigation={navigation}/>,
-					headerStyle: {
-						backgroundColor: '#4C4C4C'
-					},
-					headerTintColor: '#B29360'
-				})}/>
+  return (
+    <Stack.Navigator mode="modal">
+      <Stack.Screen
+        name="Main"
+        component={BottomNavigator}
+        options={({ navigation, route }) => ({
+          // header: () => null,
+          headerTitle: getHeaderTitle(route),
+          headerRight: () => <BotonNotificaciones navigation={navigation} />,
+          headerStyle: {
+            backgroundColor: '#4C4C4C',
+          },
+          headerTintColor: '#B29360',
+        })}
+      />
 
-			<Stack.Screen
-				name="Notificaciones"
-				component={Notificaciones}
-				options={{
-					headerTitle: 'Notificaciones',
-					headerStyle: {
-						backgroundColor: '#4C4C4C'
-					},
-					headerTintColor: '#B29360'
-				}}/>
+      <Stack.Screen
+        name="Notificaciones"
+        component={Notificaciones}
+        options={{
+          headerTitle: 'Notificaciones',
+          headerStyle: {
+            backgroundColor: '#4C4C4C',
+          },
+          headerTintColor: '#B29360',
+        }}
+      />
 
-			<Stack.Screen
-				name="NotificacionDetalle"
-				component={NotificacionesDetalle}
-				options={{
-					headerTitle: 'Notificaciones',
-					headerStyle: {
-						backgroundColor: '#4C4C4C'
-					},
-					headerTintColor: '#B29360'
-				}}/>
+      <Stack.Screen
+        name="NotificacionDetalle"
+        component={NotificacionesDetalle}
+        options={{
+          headerTitle: 'Notificaciones',
+          headerStyle: {
+            backgroundColor: '#4C4C4C',
+          },
+          headerTintColor: '#B29360',
+        }}
+      />
 
-			<Stack.Screen
-				name="GarantiasDetalle"
-				component={GarantiasDetalle}
-				options={{
-					headerTitle: 'Status',
-					headerStyle: {
-						backgroundColor: '#4C4C4C'
-					},
-					headerTintColor: '#B29360'
-				}}/>
+      <Stack.Screen
+        name="GarantiasDetalle"
+        component={GarantiasDetalle}
+        options={{
+          headerTitle: 'Status',
+          headerStyle: {
+            backgroundColor: '#4C4C4C',
+          },
+          headerTintColor: '#B29360',
+        }}
+      />
 
-			<Stack.Screen
-				name="ListaReportes"
-				component={ListaReportes}
-				options={{
-					headerTitle: 'Reportes',
-					headerStyle: {
-						backgroundColor: '#4C4C4C'
-					},
-					headerTintColor: '#B29360'
-				}}/>
-			<Stack.Screen
-				name="ListaGarantias"
-				component={ListaGarantias}
-				options={{
-					headerTitle: 'Status',
-					headerStyle: {
-						backgroundColor: '#4C4C4C'
-					},
-					headerTintColor: '#B29360'
-				}}/>
-			<Stack.Screen
-				name="ListaValoraciones"
-				component={ListaValoraciones}
-				options={{
-					headerTitle: 'Valoraciones',
-					headerStyle: {
-						backgroundColor: '#4C4C4C'
-					},
-					headerTintColor: '#B29360'
-				}}/>
-			<Stack.Screen
-				name="ListaHistorial"
-				component={ListaHistorial}
-				options={{
-					headerTitle: 'Historial',
-					headerStyle: {
-						backgroundColor: '#4C4C4C'
-					},
-					headerTintColor: '#B29360'
-				}}/>
+      <Stack.Screen
+        name="ListaReportes"
+        component={ListaReportes}
+        options={{
+          headerTitle: 'Reportes',
+          headerStyle: {
+            backgroundColor: '#4C4C4C',
+          },
+          headerTintColor: '#B29360',
+        }}
+      />
+      <Stack.Screen
+        name="ListaGarantias"
+        component={ListaGarantias}
+        options={{
+          headerTitle: 'Status',
+          headerStyle: {
+            backgroundColor: '#4C4C4C',
+          },
+          headerTintColor: '#B29360',
+        }}
+      />
+      <Stack.Screen
+        name="ListaValoraciones"
+        component={ListaValoraciones}
+        options={{
+          headerTitle: 'Valoraciones',
+          headerStyle: {
+            backgroundColor: '#4C4C4C',
+          },
+          headerTintColor: '#B29360',
+        }}
+      />
+      <Stack.Screen
+        name="ListaHistorial"
+        component={ListaHistorial}
+        options={{
+          headerTitle: 'Historial',
+          headerStyle: {
+            backgroundColor: '#4C4C4C',
+          },
+          headerTintColor: '#B29360',
+        }}
+      />
 
-			<Stack.Screen
-				name="ListaDetalleReportes"
-				component={ListaDetalleReportes}
-				options={{
-					headerTitle: 'Detalle Reporte',
-					headerStyle: {
-						backgroundColor: '#4C4C4C'
-					},
-					headerTintColor: '#B29360'
-				}}/>
+      <Stack.Screen
+        name="ListaDetalleReportes"
+        component={ListaDetalleReportes}
+        options={{
+          headerTitle: 'Detalle Reporte',
+          headerStyle: {
+            backgroundColor: '#4C4C4C',
+          },
+          headerTintColor: '#B29360',
+        }}
+      />
 
-			<Stack.Screen
-				name="DetalleReporte"
-				component={DetalleReportes}
-				options={{
-					headerTitle: 'Reporte',
-					headerStyle: {
-						backgroundColor: '#4C4C4C'
-					},
-					headerTintColor: '#B29360'
-				}}/>
-			<Stack.Screen
-				name="DetalleGarantia"
-				component={DetalleGarantias}
-				options={{
-					headerTitle: 'Status',
-					headerStyle: {
-						backgroundColor: '#4C4C4C'
-					},
-					headerTintColor: '#B29360'
-				}}/>
-			<Stack.Screen
-				name="DetalleRealizado"
-				component={DetalleRealizado}
-				options={{
-					headerTitle: 'Realizado',
-					headerStyle: {
-						backgroundColor: '#4C4C4C'
-					},
-					headerTintColor: '#B29360'
-				}}/>
-			<Stack.Screen
-				name="DetalleValoracion"
-				component={DetalleValoraciones}
-				options={{
-					headerTitle: 'Valoración',
-					headerStyle: {
-						backgroundColor: '#4C4C4C'
-					},
-					headerTintColor: '#B29360'
-				}}/>
-			<Stack.Screen
-				name="DetalleHistorico"
-				component={DetalleHistorico}
-				options={{
-					headerTitle: 'Historial',
-					headerStyle: {
-						backgroundColor: '#4C4C4C'
-					},
-					headerTintColor: '#B29360'
-				}}/>
+      <Stack.Screen
+        name="DetalleReporte"
+        component={DetalleReportes}
+        options={{
+          headerTitle: 'Reporte',
+          headerStyle: {
+            backgroundColor: '#4C4C4C',
+          },
+          headerTintColor: '#B29360',
+        }}
+      />
+      <Stack.Screen
+        name="DetalleGarantia"
+        component={DetalleGarantias}
+        options={{
+          headerTitle: 'Status',
+          headerStyle: {
+            backgroundColor: '#4C4C4C',
+          },
+          headerTintColor: '#B29360',
+        }}
+      />
+      <Stack.Screen
+        name="DetalleRealizado"
+        component={DetalleRealizado}
+        options={{
+          headerTitle: 'Realizado',
+          headerStyle: {
+            backgroundColor: '#4C4C4C',
+          },
+          headerTintColor: '#B29360',
+        }}
+      />
+      <Stack.Screen
+        name="DetalleValoracion"
+        component={DetalleValoraciones}
+        options={{
+          headerTitle: 'Valoración',
+          headerStyle: {
+            backgroundColor: '#4C4C4C',
+          },
+          headerTintColor: '#B29360',
+        }}
+      />
+      <Stack.Screen
+        name="DetalleHistorico"
+        component={DetalleHistorico}
+        options={{
+          headerTitle: 'Historial',
+          headerStyle: {
+            backgroundColor: '#4C4C4C',
+          },
+          headerTintColor: '#B29360',
+        }}
+      />
 
-			<Stack.Screen
-				name="NuevaGarantia"
-				component={NuevaGarantia}
-				options={{
-					headerTitle: 'Nuevo Reporte',
-					headerStyle: {
-						backgroundColor: '#4C4C4C'
-					},
-					headerTintColor: '#B29360'
-				}}/>
-			<Stack.Screen
-				name="Camara"
-				component={Camara}
-				options={{
-					header: () => null
-				}}/>
+      <Stack.Screen
+        name="NuevaGarantia"
+        component={NuevaGarantia}
+        options={{
+          headerTitle: 'Nuevo Reporte',
+          headerStyle: {
+            backgroundColor: '#4C4C4C',
+          },
+          headerTintColor: '#B29360',
+        }}
+      />
+      <Stack.Screen
+        name="Camara"
+        component={Camara}
+        options={{
+          header: () => null,
+        }}
+      />
 
-			<Stack.Screen
-				name="ListaDocumentos"
-				component={ListaDocumentos}
-				options={({ route }) => ({
-					headerTitle: route.params.title.toUpperCase() || '',
-					headerStyle: {
-						backgroundColor: '#4C4C4C'
-					},
-					headerTintColor: '#B29360'
-				})}/>
-			<Stack.Screen
-				name="VistaDocumento"
-				component={VistaDocumento}
-				options={({ route }) => ({
-					headerTitle: route.params.title || '',
-					headerStyle: {
-						backgroundColor: '#4C4C4C'
-					},
-					headerTintColor: '#B29360'
-				})}/>
+      <Stack.Screen
+        name="ListaDocumentos"
+        component={ListaDocumentos}
+        options={({ route }) => ({
+          headerTitle: route.params.title.toUpperCase() || '',
+          headerStyle: {
+            backgroundColor: '#4C4C4C',
+          },
+          headerTintColor: '#B29360',
+        })}
+      />
+      <Stack.Screen
+        name="VistaDocumento"
+        component={VistaDocumento}
+        options={({ route }) => ({
+          headerTitle: route.params.title || '',
+          headerStyle: {
+            backgroundColor: '#4C4C4C',
+          },
+          headerTintColor: '#B29360',
+        })}
+      />
 
-			<Stack.Screen
-				name="Usuarios"
-				component={Usuarios}
-				options={{
-					headerTitle: 'Usuarios',
-					headerStyle: {
-						backgroundColor: '#4C4C4C'
-					},
-					headerTintColor: '#B29360'
-				}}/>
+      <Stack.Screen
+        name="Usuarios"
+        component={Usuarios}
+        options={{
+          headerTitle: 'Usuarios',
+          headerStyle: {
+            backgroundColor: '#4C4C4C',
+          },
+          headerTintColor: '#B29360',
+        }}
+      />
 
-			<Stack.Screen
-				name="AgregarUsuario"
-				component={AgregarUsuario}
-				options={{
-					headerTitle: 'Agregar Inquilino',
-					headerStyle: {
-						backgroundColor: '#4C4C4C'
-					},
-					headerTintColor: '#B29360'
-				}}/>
-
-		</Stack.Navigator>
-	);
+      <Stack.Screen
+        name="AgregarUsuario"
+        component={AgregarUsuario}
+        options={{
+          headerTitle: 'Agregar Inquilino',
+          headerStyle: {
+            backgroundColor: '#4C4C4C',
+          },
+          headerTintColor: '#B29360',
+        }}
+      />
+    </Stack.Navigator>
+  );
 }
 
 export default Consumer(AppStack);
