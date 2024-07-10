313d6733324b22a090406c0a31c3f7c0d69f70d6
Itzel Ramon <itzelramonf@gmail.com>
2024-07-10 14:35:06 -0500

    remove PDF Reader

--- src/screens/documentos/documento.js
+++ src/screens/documentos/documento.js
@@ -1,53 +1,79 @@
-import React, { useState, useEffect } from 'react';
-import { useRoute } from '@react-navigation/native';
-import { View, Text, ImageBackground } from 'react-native';
-import PDFReader from 'rn-pdf-reader-js';
-
+import React, {useState, useEffect} from "react";
+import {useRoute} from "@react-navigation/native";
+import {View, Text, ImageBackground} from "react-native";
+import WebView from "react-native-webview";
 // Componentes
 
 // Styles
-import Styles from '../../styles/screens/DocumentosStyle';
-import Colores from '../../styles/colores';
+import Styles from "../../styles/screens/DocumentosStyle";
+import Colores from "../../styles/colores";
+import {Platform} from "react-native-web";
 
 function Documento() {
-	const [loading, setLoading] = useState(true);
-	const [url, setUrl] = useState('');
-	const route = useRoute();
+   const [loading, setLoading] = useState(true);
+   const [url, setUrl] = useState("");
+   const route = useRoute();
 
-	if(route.params) {
-		useEffect(() => {
-			const { data } = route.params;
-			if(data) {
-				setUrl(data.path);
-			}
-		}, [route.params]);
-	}
+   if (route.params) {
+      useEffect(() => {
+         const {data} = route.params;
+         if (data) {
+            const pdfUri =
+               Platform.OS === "ios"
+                  ? data.path
+                  : `https://docs.google.com/gview?embedded=true&url=${data.path}`;
+            setUrl(pdfUri);
+         }
+      }, [route.params]);
+   }
 
-	return (
-		<ImageBackground source={require('../../../assets/background.jpg')} style={{flex: 1, height: '100%'}}>
-			<View style={Styles.backGround}>
-				<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-					<View style={{height: loading? '80%': '100%', width: '100%'}}>
+   return (
+      <ImageBackground
+         source={require("../../../assets/background.jpg")}
+         style={{flex: 1, height: "100%"}}
+      >
+         <View style={Styles.backGround}>
+            <View
+               style={{flex: 1, alignItems: "center", justifyContent: "center"}}
+            >
+               <View style={{height: loading ? "80%" : "100%", width: "100%"}}>
+                  {loading && (
+                     <View
+                        style={{
+                           height: "20%",
+                           width: "100%",
+                           flex: 1,
+                           justifyContent: "center",
+                           alignItems: "center",
+                        }}
+                     >
+                        <Text
+                           style={{
+                              color: Colores.CardCarpetaColor,
+                              fontSize: 18,
+                           }}
+                        >
+                           Obteniendo archivo...
+                        </Text>
+                     </View>
+                  )}
 
-						{loading &&
-						<View style={{height: '20%', width: '100%', flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-							<Text style={{color: Colores.CardCarpetaColor, fontSize: 18}}>Obteniendo archivo...</Text>
-						</View>}
-
-						<View style={{height: loading? '80%': '100%', width: '100%'}}>
-							<PDFReader
-								source={{
-									uri: url,
-								}}
-								withPinchZoom	=	{true}
-								onLoadEnd = {(event) => setLoading(false)}
-							/>
-						</View>
-					</View>
-				</View>
-			</View>
-		</ImageBackground>
-	);
+                  <View
+                     style={{height: loading ? "80%" : "100%", width: "100%"}}
+                  >
+                     <WebView
+                        style={{flex: 1}}
+                        source={{
+                           uri: url,
+                        }}
+                        onLoad={() => setLoading(false)}
+                     />
+                  </View>
+               </View>
+            </View>
+         </View>
+      </ImageBackground>
+   );
 }
 
 export default Documento;
